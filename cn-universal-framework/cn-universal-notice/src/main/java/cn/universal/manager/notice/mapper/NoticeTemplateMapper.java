e3c25ec9db37842a39b6275e2a6fc3f4cce193b1
TeamBee <iot@192886.xyz>
2025-08-11 11:00:33 +0800

    del git ignore files

--- cn-universal-framework/cn-universal-notice/src/main/java/cn/universal/manager/notice/mapper/NoticeTemplateMapper.java
+++
@@ -1,6 +0,0 @@
-package cn.universal.manager.notice.mapper;
-
-import cn.universal.manager.notice.model.NoticeTemplate;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface NoticeTemplateMapper extends Mapper<NoticeTemplate> {}
--- cn-universal-persistence/src/main/java/cn/universal/persistence/mapper/IoTCertificateMapper.java
+++
@@ -1,6 +0,0 @@
-package cn.universal.persistence.mapper;
-
-import cn.universal.persistence.common.BaseMapper;
-import cn.universal.persistence.entity.IoTCertificate;
-
-public interface IoTCertificateMapper extends BaseMapper<IoTCertificate> {}
