7229bdc4eb06be2fd121daccb4330e50df4eb8ae
hex <liulh@wasu.com>
2025-08-30 21:46:56 +0800

    转移仓库

--- cn-universal-framework/cn-universal-notice/src/main/java/cn/universal/manager/notice/mapper/NoticeSendRecordMapper.java
+++
@@ -1,8 +0,0 @@
-package cn.universal.manager.notice.mapper;
-
-import cn.universal.manager.notice.model.NoticeSendRecord;
-import cn.universal.persistence.common.BaseMapper;
-
-public interface NoticeSendRecordMapper extends BaseMapper<NoticeSendRecord> {
-
-}
--- cn-universal-persistence/src/main/java/cn/universal/persistence/mapper/IoTCertificateMapper.java
+++
@@ -1,8 +0,0 @@
-package cn.universal.persistence.mapper;
-
-import cn.universal.persistence.common.BaseMapper;
-import cn.universal.persistence.entity.IoTCertificate;
-
-public interface IoTCertificateMapper extends BaseMapper<IoTCertificate> {
-
-}
